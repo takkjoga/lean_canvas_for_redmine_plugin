d0789e97ba834f7cc62a80b9732652869b482735
onodera_takumi <takumi.onodera@drecom.co.jp>
2015-08-29 02:39:35 +0900

    redmine plugin用のファイルの追加

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :lean_canvas do
+  name 'Lean Canvas plugin'
+  author 'Takumi Onodera'
+  description 'This is a plugin for generate Lean Canvas'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
